ae3d973d1801b5047f7d94b82028275d930ca01a
Louis <louieaw@gmail.com>
2013-04-10 18:25:30 -0400

    Created Rails Server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prey::Application.config.secret_token = 'c7030409e17467d250e50052b89ef97b00ca689f64216be04ababf941844c54abbb4389e92ce54adf2b4d27f9709fa7bdf015b9a1d81affc323cb3cc4358d855'
